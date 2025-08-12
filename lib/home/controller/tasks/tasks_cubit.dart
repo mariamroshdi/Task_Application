a09dc9c07d708e4a08469d4fc0189e876f20c41a
mariamroshdi <cookiesandmint333@gmail.com>
2025-08-12 13:01:21 +0300

    first commit

---
+++ lib/home/controller/tasks/tasks_cubit.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_9/home/controller/tasks/tasks_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class TasksCubit extends Cubit<TasksStates>{
+  TasksCubit() :super (TasksInitialState());
+  List<String> tasks =[];
+  TextEditingController newTask = TextEditingController();
+  void addTask(){
+    tasks.add(newTask.text);
+    newTask.clear();
+    emit(AddTaskState());
+  }
+  void deleteTask(String currentTask){
+    tasks.remove(currentTask);
+    emit(DeleteTaskState(message: "deleted Succeessfully"));
+  }
+}
---
+++ lib/home/controller/tasks/tasks_states.dart
@@ -0,0 +1,7 @@
+abstract class TasksStates {}
+class TasksInitialState extends TasksStates{}
+class DeleteTaskState extends TasksStates{
+  final String message;
+  DeleteTaskState({required this.message});
+}
+class AddTaskState extends TasksStates{}
