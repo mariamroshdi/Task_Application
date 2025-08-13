5dbc2bac3a31fd9d9123f57c5d9269d018ac5c7a
mariamroshdi <cookiesandmint333@gmail.com>
2025-08-13 12:03:07 +0300

    mainfile for dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,31 @@ class MyApp extends StatelessWidget {
     ], child: MaterialApp(
       debugShowCheckedModeBanner:false,
       title:'Flutter Demo',
+      themeMode: isDark? ThemeMode.dark: ThemeMode.light,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme:AppBarTheme(centerTitle: true, color: Colors.green),
+        textTheme: TextTheme(
+        displayLarge: TextStyle(
+          fontSize: 17,
+          color: Colors.amber,
+          fontWeight: FontWeight.bold
+        ),
+        displaySmall: TextStyle(
+          fontSize: 12,
+          color: Colors.purple,
+          fontWeight: FontWeight.bold,
+        )
+        ),
+      ),
       theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.deepOrange,
+          centerTitle: true,
+          foregroundColor: Colors.yellow,
+        ),
+        elevatedButtonTheme: 
+        ElevatedButtonThemeData(style: ElevatedButton.styleFrom()),
         buttonTheme: ButtonThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent)
         ),
@@ -37,7 +61,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
           brightness: Brightness.light,
           onPrimary: Colors.blue,
-          seedColor: Colors.black),
+          seedColor: Colors.red),
         useMaterial3: true,
       ),
       home: HomeScreen(),
